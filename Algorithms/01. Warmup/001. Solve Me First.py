de36e13402b8fed7f65d2ee751fc57973cc076ba
dispe1 <lkjim0757@naver.com>
2019-08-27 18:07:19 +0900

    add solution of solve me first

---
+++ Algorithms/01. Warmup/001. Solve Me First.py
@@ -0,0 +1,11 @@
+# Problem: https://www.hackerrank.com/challenges/solve-me-first/problem
+# Difficulty : Easy
+# Score : 1
+
+def solveMeFirst(a,b):
+    return a+b
+
+num1 = int(input())
+num2 = int(input())
+res = solveMeFirst(num1,num2)
+print(res)
