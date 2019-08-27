bebf9554464e6e301a398ebd1e5a88850ce57cab
dispe1 <lkjim0757@naver.com>
2019-08-27 18:01:04 +0900

    add generateReadme.py

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-Hackerrank-Solutions
+# Solutions to Hackerrank practice problems
+This repository contains 7 solutions to Hackerrank practice problems with Python 3.
+
+Updated daily :) If it was helpful please press a star.
+
+[![GitHub last commit](https://img.shields.io/github/last-commit/dispe1/Hackerrank-Solutions.svg)](https://github.com/dispe1/Hackerrank-Solutions)
+[![GitHub commit activity the past week, 4 weeks, year](https://img.shields.io/github/commit-activity/y/dispe1/Hackerrank-Solutions.svg)](https://github.com/dispe1/Hackerrank-Solutions)
+[![GitHub repo size in bytes](https://img.shields.io/github/repo-size/dispe1/Hackerrank-Solutions.svg)](https://github.com/dispe1/Hackerrank-Solutions)
+[![GitHub stars](https://img.shields.io/github/stars/dispe1/Hackerrank-Solutions.svg)](https://github.com/dispe1/Hackerrank-Solutions)
+
+- Algorithms
+    02. Implementation
+        - Sock Merchant | [Problem](https://www.hackerrank.com/challenges/sock-merchant/problem) | [Solution](https://github.com/dispe1/Hackerrank-Solutions/blob/master/Algorithms/02.%20Implementation/001.%20Sock%20Merchant.py) | Difficulty: Easy | Score: 10
+        - Counting Valleys | [Problem](https://www.hackerrank.com/challenges/counting-valleys/problem) | [Solution](https://github.com/dispe1/Hackerrank-Solutions/blob/master/Algorithms/02.%20Implementation/002.%20Counting%20Valleys.py) | Difficulty: Easy | Score: 15
+        - Jumping on the Clouds | [Problem](https://www.hackerrank.com/challenges/jumping-on-the-clouds/problem) | [Solution](https://github.com/dispe1/Hackerrank-Solutions/blob/master/Algorithms/02.%20Implementation/003.%20Jumping%20on%20the%20Clouds.py) | Difficulty: Easy | Score: 20
+        - Repeated String | [Problem](https://www.hackerrank.com/challenges/repeated-string/problem) | [Solution](https://github.com/dispe1/Hackerrank-Solutions/blob/master/Algorithms/02.%20Implementation/004.%20Repeated%20String.py) | Difficulty: Easy | Score: 20
+    09. Constructive Algorithms
+        - New Year Chaos | [Problem](https://www.hackerrank.com/challenges/new-year-chaos/problem) | [Solution](https://github.com/dispe1/Hackerrank-Solutions/blob/master/Algorithms/09.%20Constructive%20Algorithms/001.%20New%20Year%20Chaos.py) | Difficulty: Medium | Score: 40
+- Data Structures
+    01. Arrays
+        - 2D Array - DS | [Problem](https://www.hackerrank.com/challenges/2d-array/problem) | [Solution](https://github.com/dispe1/Hackerrank-Solutions/blob/master/Data%20Structures/01.%20Arrays/001.%202D%20Array%20-%20DS.py) | Difficulty: Easy | Score: 15
+        - Left Rotation | [Problem](https://www.hackerrank.com/challenges/ctci-array-left-rotation/problem) | [Solution](https://github.com/dispe1/Hackerrank-Solutions/blob/master/Data%20Structures/01.%20Arrays/002.%20Left%20Rotation.py) | Difficulty: Easy | Score: 20
---
+++ generateReadme.py
@@ -0,0 +1,69 @@
+
+import os
+import functools
+
+
+def getFoldersNames(path):
+    folders = []
+    for item in os.listdir(path):
+        if not os.path.isfile(item) and item not in ('.git', '.idea'):
+            folders.append(item)
+    return folders
+
+
+def getFilesNames(path):
+    files = os.listdir(path)
+    return files
+
+
+def getProblemURLandScore(path):
+    inFile = open(path, 'r')
+    url = inFile.readline().split()[-1]
+    difficulty = inFile.readline().split()[-1]
+    score = inFile.readline().split()[-1]
+    inFile.close()
+    return url, difficulty, score
+
+
+def getTotalNumberOfProblems():
+    totalNumber = 0
+    folders = getFoldersNames(os.getcwd())
+    for folder in folders:
+        subfolders = getFoldersNames(os.path.join(os.getcwd(), folder))
+        for subfolder in subfolders:
+            totalNumber += len(getFilesNames(os.path.join(os.getcwd(), folder, subfolder)))
+    return totalNumber
+
+
+readmeFile = open('README.md', 'w')
+readmeFile.write('# Solutions to Hackerrank practice problems\n')
+readmeFile.write('This repository contains ' + str(getTotalNumberOfProblems()) + ' solutions to Hackerrank practice problems with Python 3.\n')
+readmeFile.write('\n')
+readmeFile.write('Updated daily :) If it was helpful please press a star.\n')
+readmeFile.write('\n')
+readmeFile.write('[![GitHub last commit](https://img.shields.io/github/last-commit/dispe1/Hackerrank-Solutions.svg)](https://github.com/dispe1/Hackerrank-Solutions)\n')
+readmeFile.write('[![GitHub commit activity the past week, 4 weeks, year](https://img.shields.io/github/commit-activity/y/dispe1/Hackerrank-Solutions.svg)](https://github.com/dispe1/Hackerrank-Solutions)\n')
+readmeFile.write('[![GitHub repo size in bytes](https://img.shields.io/github/repo-size/dispe1/Hackerrank-Solutions.svg)](https://github.com/dispe1/Hackerrank-Solutions)\n')
+readmeFile.write('[![GitHub stars](https://img.shields.io/github/stars/dispe1/Hackerrank-Solutions.svg)](https://github.com/dispe1/Hackerrank-Solutions)\n')
+readmeFile.write('\n')
+
+folders = getFoldersNames(os.getcwd())
+for folder in folders:
+    readmeFile.write('- ' + folder + '\n')
+    subfolders = getFoldersNames(os.path.join(os.getcwd(), folder))
+    for subfolder in subfolders:
+        readmeFile.write('    ' + subfolder + '\n')
+        files = getFilesNames(os.path.join(os.getcwd(), folder, subfolder))
+        for file in files:
+            url, difficulty, score = getProblemURLandScore(os.path.join(os.getcwd(), folder, subfolder, file))
+            readmeFile.write('        - ' + "".join(file.split(".")[1:-1])[1:]
+                  + ' | [Problem](' + url
+                  + ')'
+                  + ' | [Solution]'
+                  + '(https://github.com/dispe1/Hackerrank-Solutions/blob/master/'
+                  + folder.replace(' ', '%20') + '/' + subfolder.replace(' ', '%20') + '/'
+                  + file.replace(' ', '%20') + ')'
+                  + ' | Difficulty: ' + difficulty
+                  + ' | Score: ' + str(score) + '\n')
+
+readmeFile.close()
