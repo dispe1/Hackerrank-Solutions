08ebda265c49be249941d70fab5eb3403693fea2
dispe1 <lkjim0757@naver.com>
2019-09-06 19:31:34 +0900

    add solution of Print in Reverse

---
+++ Data Structures/02. Linked Lists/006. Print in Reverse.py
@@ -0,0 +1,66 @@
+# Problem: https://www.hackerrank.com/challenges/print-the-elements-of-a-linked-list-in-reverse/problem
+# Difficulty : Easy
+# Score : 5
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+
+        self.tail = node
+
+def print_singly_linked_list(node, sep):
+    while node:
+        print(node.data, end='')
+
+        node = node.next
+
+        if node:
+            print(sep, end='')
+
+def reversePrint(head):
+    r = []
+    while head != None:
+        r.append(head.data)
+        head = head.next
+    r.reverse()
+
+    for i in r:
+        print(i)
+
+
+if __name__ == '__main__':
+    tests = int(input())
+
+    for tests_itr in range(tests):
+        llist_count = int(input())
+
+        llist = SinglyLinkedList()
+
+        for _ in range(llist_count):
+            llist_item = int(input())
+            llist.insert_node(llist_item)
+
+        reversePrint(llist.head)
