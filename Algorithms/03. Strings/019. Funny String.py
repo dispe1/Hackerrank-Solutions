9095373c87beb3a123b5cc03ccb54025986fbc86
dispe1 <lkjim0757@naver.com>
2019-10-10 21:21:50 +0900

    add solution of Funny String.py

---
+++ Algorithms/03. Strings/019. Funny String.py
@@ -0,0 +1,30 @@
+# Problem: https://www.hackerrank.com/challenges/sfunny-string/problem
+# Difficulty : Easy
+# Score : 25
+
+import os
+
+def funnyString(s):
+    r = s[::-1]
+
+    s = [abs(ord(s[i]) - ord(s[i+1])) for i in range(len(s)-1)]
+    r = [abs(ord(r[i]) - ord(r[i+1])) for i in range(len(r)-1)]
+
+    if s == r:
+        return "Funny"
+
+    return "Not Funny"
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input())
+
+    for q_itr in range(q):
+        s = input()
+
+        result = funnyString(s)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
