685e7ded115db236dd02b498433ae7e258ce7309
dispe1 <lkjim0757@naver.com>
2019-11-26 21:45:00 +0900

    Add solution of 30 Days of Code: Day 26

--- Tutorial/02. 30 Days of Code/026. Day 25; Running Time and Complexity.py
+++ Tutorial/02. 30 Days of Code/026. Day 25; Running Time and Complexity.py
@@ -1,3 +1,22 @@
 # Problem: https://www.hackerrank.com/challenges/30-running-time-and-complexity/problem
 # Difficulty : Easy
 # Score : 30
+
+import math
+
+def check_prime(num):
+    if num == 1:
+        return "Not prime"
+    sq = int(math.sqrt(num))
+    for x in range(2, sq+1):
+        if num % x == 0:
+            return "Not prime"
+    return "Prime"
+
+n = int(input())
+ar = []
+for i in range(n):
+    ar.append(int(input()))
+
+for i in ar:
+    print(check_prime(i))
